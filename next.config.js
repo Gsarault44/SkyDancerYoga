35d338b3177a77279fbdd46c96bb059046a1c9af
Gsarault44 <gregsarault@gmail.com>
2023-06-22 22:44:56 -0400

    new build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+const globImporter = require('node-sass-glob-importer');
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    importer: globImporter(),
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
