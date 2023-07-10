4a53febb3bee06142d4716c341ac57c8630eea31
Gsarault44 <gregsarault@gmail.com>
2023-07-09 22:57:57 -0400

    about

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@styles/app.scss';
-import { Nanum_Gothic } from 'next/font/google'
+import { Nanum_Gothic, Poppins } from 'next/font/google'
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const inter = Nanum_Gothic({ weight: "400", subsets: ['latin'] })
+const pop = Poppins({ weight: "200", subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <>
       <style jsx global>{`
-        html * {
+        html nav * {
           font-family: ${inter.style.fontFamily};
+        }
+        html body * {
+          font-family: ${pop.style.fontFamily};
         }
       `}</style>
       <header className={`header ${router.pathname == '/' && 'home'}`}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,6 +59,7 @@ export default function Home() {
               />
             </div>
             <div>
+              <h1>Dance in the sky, simply for the delight of it and become one with it all</h1>
               <Image
                 src="/hero/dancer-pose.jpg"
                 alt="Fitness"
@@ -180,6 +181,22 @@ export default function Home() {
               />
             </div>
           </Slider>
+        <section className="about">
+          <Image
+            src="/waterfall.jpg"
+            alt="Fitness"
+            width={0}
+            height={0}
+            sizes='100vw'
+            style={{ width: '100%', height: '100vh', objectFit: 'cover' }}
+            priority
+          />
+          <div className="about__content">
+            <p>Wendy’s fascination with the human body began at an early age as a dancer.  She still marvels at the sophisticated design of this mortal vehicle and seeks to teach her students practical tools to enhance their life journey both physically and spiritually.</p>
+            <p>Wendy’s classes reflect her belief that properly aligning the body allows us to align with the universe and this alignment benefits both body and soul.  She offers students a slow flow of poses with an opportunity to slow down, stretch-out and re-connect with their innate goodness.  Wendy completed her yoga teacher training with Suzie Hurley at Willow Street Yoga Center in Takoma Park, MD in 2009. She is deeply grateful to the community at Willow Street for their support, encouragement and wisdom.</p>
+            <p>Wendy holds a B.S. in Physical Education and is a certified Exercise Physiologist through the American College of Sports Medicine.  She has been teaching, training and coaching yoga, fitness and wellness since 2002.</p>
+          </div>
+        </section>
       </main>
     </>
   )
