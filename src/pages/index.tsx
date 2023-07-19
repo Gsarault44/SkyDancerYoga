58c147fd3edce6a720b50d4ba6c109c837727a01
Gsarault44 <gregsarault@gmail.com>
2023-07-18 22:21:46 -0400

    adding retreat to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import Slider from "react-slick";
 
 export default function Home() {
@@ -12,8 +13,7 @@ export default function Home() {
     slidesToScroll: 1,
     cssEase: 'cubic-bezier(0.600, -0.280, 0.735, 0.045)',
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 2500,
   };
   return (
     <>
@@ -115,18 +115,7 @@ export default function Home() {
                 style={{ width: '100%', height: '100vh', objectFit: 'cover' }}
                 priority
               />
-              <h1 className="slider-text">“Wendy is a gifted yoga instructor -- enthusiastic and encouraging -- who has a huge repertoire of moves she adapts to any student level. As someone who has taken her classes for more than 10 years, I remain awed by her creativity and energy.”&nbsp;<span>—Marilyn M.</span></h1>
-            </div>
-            <div>
-              <Image
-                src="/hero/warrior-II-bluish.jpg"
-                alt="Fitness"
-                width={0}
-                height={0}
-                sizes='100vw'
-                style={{ width: '100%', height: '100vh', objectFit: 'cover' }}
-                priority
-              />
+              <h1 className="slider-text">“Wendy is a gifted yoga instructor -- enthusiastic and encouraging -- who has a huge repertoire of moves she adapts to any student level. As someone who has taken her classes for more than 10 years, I remain awed by her creativity and energy.”&nbsp;<span>—Marilyn M.</span></h1>  
             </div>
             <div>
               <Image
@@ -152,7 +141,18 @@ export default function Home() {
             </div>
             <div>
               <Image
-                src="/hero/wendy-yoga-3.jpg"
+                src="/waterfall.jpg"
+                alt="Fitness"
+                width={0}
+                height={0}
+                sizes='100vw'
+                style={{ width: '100%', height: '100vh', objectFit: 'cover' }}
+                priority
+              />
+            </div>
+            <div>
+              <Image
+                src="/hero/warrior-II-bluish.jpg"
                 alt="Fitness"
                 width={0}
                 height={0}
@@ -186,7 +186,7 @@ export default function Home() {
           </Slider>
         <section className="about" id="about">
           <Image
-            src="/waterfall.jpg"
+            src="/hero/wendy-yoga-3.jpg"
             alt="Fitness"
             width={0}
             height={0}
@@ -198,6 +198,15 @@ export default function Home() {
             <p>Wendy’s fascination with the human body began at an early age as a dancer.  She still marvels at the sophisticated design of this mortal vehicle and seeks to teach her students practical tools to enhance their life journey both physically and spiritually.</p>
             <p>Wendy’s classes reflect her belief that properly aligning the body allows us to align with the universe and this alignment benefits both body and soul.  She offers students a slow flow of poses with an opportunity to slow down, stretch-out and re-connect with their innate goodness.  Wendy completed her yoga teacher training with Suzie Hurley at Willow Street Yoga Center in Takoma Park, MD in 2009. She is deeply grateful to the community at Willow Street for their support, encouragement and wisdom.</p>
             <p>Wendy holds a B.S. in Physical Education and is a certified Exercise Physiologist through the American College of Sports Medicine.  She has been teaching, training and coaching yoga, fitness and wellness since 2002.</p>
+          </div>
+        </section>
+        <section className="home-retreats">
+          <div className="home-retreats__inner">
+            <h2>Join us on a retrat</h2>
+            <p>Embark on a soul-renewing journey at our stunning resort. Join our destination yoga class amidst breathtaking vistas and tranquil beaches. Reconnect with your inner self, guided by experienced instructors. Experience transformative moments of self-discovery and blissful connections with like-minded souls. Unwind, rejuvenate, and elevate your well-being in paradise. Come, find serenity and enchantment at our destination yoga retreat.</p>
+            <Link href="/retreats" className="button">
+              Show me more
+            </Link>
           </div>
         </section>
       </main>
